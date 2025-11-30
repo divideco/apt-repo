1d9816ffa218b4ded1981371394dbda4f317b784
Micah John <micahvdm@gmail.com>
2025-11-30 13:07:11 +0200

    base setup

---
+++ dpkg.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+for i in $(ls); do
+    if [ "$i" = "docs" ]; then
+        continue
+    fi
+	if [ -d $i ]; then
+		cd $i
+		if [ -e SKIP ]; then
+			echo Copying $i deb file
+			cp -f *.deb ../pool/main/
+			rm SKIP
+		else
+			echo Skipping copying $i due to no SKIP file.
+		fi
+		cd ..
+	fi
+done
+
+dpkg-scanpackages pool /dev/null > dists/stable/main/binary-amd64/Packages
+gzip -k -f dists/stable/main/binary-amd64/Packages
+
+git add .
+git commit -m "Updating APT package $1"
