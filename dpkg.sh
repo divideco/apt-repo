97dbc4420bfdefd85e00edea557c80ff3df2bed2
Micah John <micahvdm@gmail.com>
2025-12-01 16:16:47 +0000

    Updating APT package

--- dpkg.sh
+++ dpkg.sh
@@ -2,25 +2,25 @@
 
 set -e
 
-for i in $(ls); do
-    if [ "$i" = "docs" ]; then
-        continue
-    fi
-	if [ -d $i ]; then
-		cd $i
-		if [ -e SKIP ]; then
-			echo Copying $i deb file
-			cp -f *.deb ../pool/main/
-			rm SKIP
-		else
-			echo Skipping copying $i due to no SKIP file.
-		fi
-		cd ..
-	fi
-done
+#for i in $(ls); do
+#    if [ "$i" = "docs" ]; then
+#        continue
+#    fi
+#	if [ -d $i ]; then
+#		cd $i
+#		if [ -e SKIP ]; then
+#			echo Copying $i deb file
+#			cp -f ../../*.deb pool/main/
+#			rm SKIP
+#		else
+#			echo Skipping copying $i due to no SKIP file.
+#		fi
+#		cd ..
+#	fi
+#done
 
-dpkg-scanpackages pool /dev/null > dists/stable/main/binary-amd64/Packages
-gzip -k -f dists/stable/main/binary-amd64/Packages
+dpkg-scanpackages pool /dev/null > dists/stable/main/binary-arm64/Packages
+gzip -k -f dists/stable/main/binary-arm64/Packages
 
 git add .
 git commit -m "Updating APT package $1"
